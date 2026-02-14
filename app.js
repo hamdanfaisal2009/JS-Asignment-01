8bc8eb1b0810ddd13588f933ed8228271031b567
hamdanfisal2009 <hamdanfaisal2009@gmail.com>
2026-02-15 03:27:54 +0500

    JS Assignment has comeleted

---
+++ app.js
@@ -0,0 +1,101 @@
+// Using camel case for variable naming convention
+
+var myName = "My name is Hamdan ";
+var myage = "I am " + 17 + " years old.";
+var myCity = "My city is Karachi.";
+var studentCondition ="MY student condition is | " + true;
+
+console.log(myName);
+console.log(myage);
+console.log(myCity);
+console.log(studentCondition);
+
+console.log("-----------------------------");
+
+
+// Specifieng the data types of variables
+
+var userName = "Hamdan"; // this data type name is string because of double quotes.
+var age = 17; // this data type name is number because of numeric value.
+var isLoggedIn = true; // this data type name is boolean because of true / false value.
+var score = "98"; // this dat type name is string because of double quotes.
+
+
+// Mathematic operations with variables
+
+var Addition = 10 + 20;
+var Subtraction = 35 - 10;
+var Multiplication = 9 * 6;
+
+console.log(` ${Addition} | This is the result of Addition var. `);
+console.log(` ${Subtraction} | This is the result of Subtraction var. `);
+console.log(` ${Multiplication} | This is the result of Multiplication var. `);
+
+console.log("-----------------------------");
+
+// Paranthises in mathematic operations
+
+var result1 = 5 + 5 * 2; // my rediction is answer is 15 because of BODMAS rule. First multiplication will be done and then addition.
+console.log(result1);
+
+
+var result2 = (5 + 5) * 2; // my rediction is answer is 20 because of BODMAS rule. First addition will be done because of paranthises and then multiplication.
+console.log(result2);
+
+console.log("-----------------------------");
+
+// Alert mesage using only variables
+
+var alertMessage =  "Hello!  MY name is Hamdan.";
+var alertMessage2 = "And I am 17 years old.";
+
+alert(alertMessage);
+alert(alertMessage2);
+
+// fixing the errors in the code    
+
+// this code with erorrs and mistakes.
+// var 1name = "Ali";
+// var age = "20;
+// console.log Age;
+// alert Hello World;
+
+// this code is fixed without any errors and mistakes.
+// var name = "Ali";
+// var secage = 20;
+// console.log (age);
+// alert("Hello World.");
+
+
+// Mini profie progmae using variables
+
+var name = "Hamdan";
+var myAge = 17;
+var favNumber = 23;
+var isStudent = true;
+
+console.log(`My name is ${name}.
+I am ${myAge} years old.
+My favourite number is ${favNumber}. 
+It is ${isStudent} that I am a student.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
